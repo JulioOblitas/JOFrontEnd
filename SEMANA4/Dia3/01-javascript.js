bfe3a404526195ea1030811987dfae55ea06debd
JulioOblitas <89611439+JulioOblitas@users.noreply.github.com>
2021-09-19 10:26:44 -0500

    cambios

---
+++ SEMANA4/Dia3/01-javascript.js
@@ -0,0 +1,10 @@
+console.log("hola mundo");
+var nombre = "Julio";
+var nombre2 = "Martin";
+var edad = 18;
+console.log(edad);
+
+ var esdenoche = true;
+ console.log(esdenoche);
+
+ console.log( typeof esdenoche);
---
+++ SEMANA4/Dia3/02-aritmetica.js
@@ -0,0 +1,6 @@
+let a = 100;
+let b = 4;
+let suma = a +b; 
+console.log("Suma" , suma)
+
+console.log(1000+50);
